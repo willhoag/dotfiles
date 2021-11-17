5e0a1c6eb264a4994795578dfcdcc77b0fed92f5
Will Hoag <dev@willhoag.com>
2021-11-17 14:10:02 -0800

    fish: add serverless things

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,10 @@ if type -q nvm
     nvm use default
 end
 
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /Users/willhoag/.nvm/versions/node/v10.21.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/willhoag/.nvm/versions/node/v10.21.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /Users/willhoag/.nvm/versions/node/v10.21.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /Users/willhoag/.nvm/versions/node/v10.21.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
